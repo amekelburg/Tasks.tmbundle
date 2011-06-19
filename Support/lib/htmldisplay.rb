aed97e9201299f4f5f3a4651cbd35a6221f55bc0
Alexander Mekelburg <alexmek@alexander-mekelburgs-computer.local>
2011-06-19 02:27:13 -0400

    integrate the tlp gem

---
+++ Support/lib/htmldisplay.rb
@@ -0,0 +1,19 @@
+CSS = "
+<style type='text/css'>
+
+
+.tl-blank-task {
+  display:none;
+}
+
+.tl-lineNumber {
+  display:none;
+}
+
+
+</style>
+"
+
+
+
+
---
+++ Support/lib/requirements.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+#require '/Volumes/AMM/Work/planner/task_list_parser/lib/task_list_parser'
+require 'task_list_parser'
+
+
+
+#require 'rubygems'
+
+#require 'task_list_parser'
+
+#require '/Volumes/AMM/Work/planner/task_list_parser/lib/task_list_parser'
+
